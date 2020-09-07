d21031721ba9f88fd62056f7f53d49fcef70159b
Ayush Agrawal <ayushagrawal@Ayushs-MacBook-Pro.local>
2020-09-07 13:44:54 -0700

    update

--- kick_20e_3.cpp
+++ kick_20e_3.cpp
@@ -18,30 +18,30 @@ using namespace std;
 
 ll mul(ll x, ll y)
 {
-  ll ans = 1;
+    ll ans = 1;
 
-  while (y > 0)
-  {
-    if (y & 1)
-      ans = (ans * x) % MOD;
-    y /= 2;
-    x = (x * x) % MOD;
-  }
+    while (y > 0)
+    {
+        if (y & 1)
+            ans = (ans * x) % MOD;
+        y /= 2;
+        x = (x * x) % MOD;
+    }
 
-  return ans;
+    return ans;
 };
 
 /******************************************************************************************************/
 
-ll enj[100005],rem[100005];
+ll enj[100005], rem[100005];
 
 ll bit[100006];
 void update(ll x, ll val)
 {
-    while(x<100003)
+    while (x < 100003)
     {
-        bit[x]+=val;
-        x+=x&(-x);
+        bit[x] += val;
+        x += x & (-x);
     }
     return;
 }
@@ -49,67 +49,68 @@ void update(ll x, ll val)
 ll query(ll x)
 {
     ll ans = 0;
-    while(x>0)
+    while (x > 0)
     {
-        ans+=bit[x];
-        x-=x&(-x);
+        ans += bit[x];
+        x -= x & (-x);
     }
     return ans;
 }
 
 int main()
 {
-    ll t,g=1;
+    ll t, g = 1;
     s(t);
 
-    while(t--)
+    while (t--)
     {
-        ll n,i,sum=0,ans=0,dd=0;
+        ll n, i, sum = 0, ans = 0, dd = 0;
         s(n);
-        memset(bit,0,sizeof bit);
+        memset(bit, 0, sizeof bit);
 
-        for(i=1;i<=n;i++)
+        for (i = 1; i <= n; i++)
         {
-            s(enj[i]); s(rem[i]);
-            update(i,enj[i]);
-            sum+=enj[i];
+            s(enj[i]);
+            s(rem[i]);
+            update(i, enj[i]);
+            sum += enj[i];
         }
 
-        set<pair<ll,ll> > safe;
+        set<pair<ll, ll>> safe;
         set<ll> bad;
 
-        for(i=1;i<=n;i++)
+        for (i = 1; i <= n; i++)
         {
-            if(sum-enj[i]>=rem[i])
-                safe.insert(_mp(sum-enj[i]-rem[i],i));
+            if (sum - enj[i] >= rem[i])
+                safe.insert(_mp(sum - enj[i] - rem[i], i));
             else
                 bad.insert(i);
         }
 
-        ll rem = 0,top,cnt=0,mid;
-        pair<ll,ll> pp;
-        while(!bad.empty())
+        ll rem = 0, top, cnt = 0, mid;
+        pair<ll, ll> pp;
+        while (!bad.empty())
         {
             top = *bad.begin();
             bad.erase(top);
 
-            mid = query(n) + query(top-1);
-            if(mid>ans)
+            mid = query(n) + query(top - 1);
+            if (mid > ans)
             {
                 ans = mid;
                 dd = cnt;
             }
             ++cnt;
             // ans = max(ans, query(n) + query(top-1));
-            update(top,-enj[top]);
-            rem+=enj[top];
+            update(top, -enj[top]);
+            rem += enj[top];
             // cout<<top<<": "<<rem<<"\n";
-            
-            while(!safe.empty())
+
+            while (!safe.empty())
             {
                 pp = *safe.begin();
                 // cout<<pp.ff<<"--"<<pp.ss<<"\n";
-                if(pp.ff<rem)
+                if (pp.ff < rem)
                 {
                     bad.insert(pp.ss);
                     safe.erase(pp);
@@ -119,11 +120,11 @@ int main()
             }
         }
 
-        cout<<"Case #"<<g<<": ";
-        if(safe.size()!=0)
-            cout<<n-safe.size()<<" INDEFINITELY\n";
+        cout << "Case #" << g << ": ";
+        if (safe.size() != 0)
+            cout << n - safe.size() << " INDEFINITELY\n";
         else
-            cout<<dd<<" "<<ans<<"\n";
+            cout << dd << " " << ans << "\n";
 
         ++g;
     }
